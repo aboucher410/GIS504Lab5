1efcf5515abe9f02247f4585b283956b1c4627e0
aboucher410 <46583858+aboucher410@users.noreply.github.com>
2019-03-23 18:42:13 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -13,7 +13,62 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1IjoiYWIxMCIsImEiOiJjanN3ZXZua3cwZ3ppNDNwcjd4aGRpMGR0In0.FPuUGMHZEG23LWr-NWhfbA' //this is a generic access token, but when you deploy projects of your own, you must get a unique key that is tied to your Mapbox account
 }).addTo(map);
 
+var map_map;
+var map_markers = [];
+var map_polylines = [];
+var map_route_waypoints = [];
+var map_feature_group;
 
+(function() {
+    map_map = L.map('map', {
+        center: [52.5,13.4],
+        attributionControl: false
+    });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'jaygoodswp.0a28k881',
+        accessToken: 'pk.eyJ1IjoiamF5Z29vZHN3cCIsImEiOiJjaW5scm92bjIweXh2dHRtM2N3Nmdja3QwIn0.tQz7VNzTgdlEuibA5swS3w'
+    }).addTo(map_map);
+})();
+
+(function() {
+    var routing = [];
+
+    // [START] the following repeats 10 times (but with different waypoints)
+map_route_waypoints.push([[48.427626,10.877655],[48.427626,10.877655],[48.428817,10.877530],[48.428794,10.877490],[48.428794,10.877490],[48.429427,10.876769],[48.429993,10.875997],[48.429985,10.874054],[48.429983,10.871665],[48.429978,10.869169],[48.429975,10.866704],[48.429880,10.865278],[48.428253,10.865594],[48.426140,10.866007],[48.424412,10.865985],[48.424267,10.865933],[48.424267,10.865933],[48.424010,10.865870],[48.423200,10.867021],[48.420123,10.868246],[48.417399,10.869749],[48.415272,10.868973],[48.415029,10.864935],[48.415558,10.857768],[48.416302,10.848967],[48.417230,10.838084],[48.419207,10.815853],[48.419956,10.805343],[48.417408,10.746530],[48.416791,10.726497],[48.417313,10.717214],[48.417980,10.707847],[48.418672,10.698317],[48.419447,10.689150],[48.420022,10.680101],[48.418308,10.671601],[48.414469,10.663102],[48.412280,10.653700],[48.411017,10.644776],[48.409779,10.636280],[48.410072,10.625954],[48.410185,10.614270],[48.409880,10.601512],[48.409742,10.589707],[48.407958,10.577822],[48.406187,10.568996],[48.404042,10.560529],[48.401143,10.553553],[48.399954,10.545541],[48.401261,10.536627],[48.403610,10.528194],[48.405992,10.519216],[48.408022,10.509773],[48.409374,10.499818],[48.410475,10.489759],[48.411363,10.481394],[48.412196,10.474118],[48.413007,10.466547],[48.413455,10.459332],[48.413090,10.450934],[48.412910,10.441510],[48.412711,10.431478]])
+    routing.push(L.Routing.control({
+        waypoints: map_route_waypoints[0],
+        show: false,
+        waypointMode: 'snap',
+        fitSelectedRoutes: false,
+        createMarker: function() {}
+    }).addTo(map_map));
+
+    routing[0].on('routeselected', function(e) {
+        map_polylines.push(L.polyline(e.route.coordinates, {
+            stroke: true,
+            color: '#FF69B4',
+            weight: 5
+        }));
+        map_feature_group.addLayer(map_polylines[0]);
+        map_map.removeControl(routing[0]);
+    });
+
+    routing[0].on('routingerror', function(error) {
+        map_polylines.push(L.polyline(map_route_waypoints[0], {
+            stroke: true,
+            color: '#000',
+            weight: 5
+        }));
+        map_feature_group.addLayer(map_polylines[0]);
+        map_map.removeControl(routing[0]);
+    });
+    // [END] the preceding repeats 10 times
+
+    map_feature_group.addTo(map_map);
+    map_map.fitBounds(map_feature_group.getBounds());
+})();
 //the below JS code takes advantage of the Geolocate API as it is incorporated in the Leaflet JS API with the locate method
 function onLocationFound(e) { //this function does three things if the location is found: it defines a radius variable, adds a popup to the map, and adds a circle to the map.
 
